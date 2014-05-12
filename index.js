09501a80d6a2726c78ec2a890a800f4452fbcadb
tjmehta <tejesh.mehta@gmail.com>
2014-05-12 00:03:57 -0700

    fixed merge issue

--- index.js
+++ index.js
@@ -1,74 +1,9 @@
 var createCount = require('callback-count');
 
 var flow = module.exports = {
-  series: function (/* middlewares */) {
-    var args = Array.prototype.slice.call(arguments);
-    return function (req, res, next) {
-      var middlewares = args.slice(); // copy
-      step(middlewares.shift());
-      var error;
-      function step (mw) {
-        if (mw) {
-          if (error) {
-            mw(error, req, res, nextStep);
-          }
-          else {
-            mw(req, res, nextStep);
-          }
-        }
-        else {
-          next(error); // done
-        }
-      }
-      function nextStep (err) {
-        if (err) {
-          error = err;
-          middlewares = middlewares.filter(lengthOf(4));
-        }
-        step(middlewares.shift()); // continue
-      }
-    };
-  },
-  parallel: function (/* middlewares */) {
-    var args = Array.prototype.slice.call(arguments);
-    return function (req, res, next) {
-      var count = createCount(next);
-      var middlewares = args.slice(); // copy
-
-      middlewares.forEach(function () {
-        count.inc(); // inc first just in case the middlewares are sync
-      });
-      middlewares.forEach(function (mw) {
-        mw(req, res, count.next);
-      });
-    };
-  },
-  or: function (/* middlewares */) {
-    var args = Array.prototype.slice.call(arguments);
-    return function (req, res, next) {
-      var middlewares = args.slice(); // copy
-      var firstErr;
-
-      step(middlewares.shift());
-      function step (mw) {
-        if (mw) {
-          mw(req, res, nextStep);
-        }
-        else {
-          next(firstErr); // done w/ err or no mw
-        }
-      }
-      function nextStep (err) {
-        if (err) {
-          firstErr = firstErr || err;
-          step(middlewares.shift()); // continue
-        }
-        else {
-          next(); // done
-        }
-      }
-    };
-  },
+  series: require('./lib/series'),
+  parallel: require('./lib/parallel'),
+  or: require('./lib/or'),
   next: function (req, res, next) {
     next();
   },
